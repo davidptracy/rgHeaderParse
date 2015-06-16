f880bd82fb7de961470f4a9d00ef1d9446d7ff6e
David Tracy <davidptracy@gmail.com>
2015-06-16 14:15:57 -0400

    added device parsing logic

--- server.js
+++ server.js
@@ -22,10 +22,34 @@ app.get('/ua', function(req, res){
 
 	var userAgent = req.headers['user-agent'];
 
-	console.log(req.headers);
+	// regex to parse info between parentehsis' into an array
+	var userInfo = userAgent.match(/\(([^)]+)\)/g); 
+	
+	// grab the first item in the array, that's the device info, and split it at '('
+	var deviceInfo = userInfo[0].split('(');
+	
+	// deviceInfo is also an array, split the second item at the ';'
+	deviceInfo = deviceInfo[1].split(';');
+	
+	// cast the first item in the array as the DeviceType
+	var deviceType = deviceInfo[0];
+	
+	// cast the second item in the array as the Device OS after removing some extra chars and whitespace
+	var deviceOS = deviceInfo[1].replace(/\)/g, '');
+	deviceOS = deviceOS.trim();
 
+	console.log('Device Type: ' + deviceType);
+	console.log('Device OS: '+ deviceOS);
+
+	if (deviceType == 'Macintosh'){
+		console.log("It's a Mac!");
+	} else if (deviceType == 'iPhone'){
+		console.log("It's an iPhone!");
+	} else {
+		console.log("I don't know what it is!")
+	}
+
+	// console.log(req.headers);
     res.send('user ' + userAgent);
-
-    console.log ("got request from > " + userAgent);
 
 });
